52d45765c615d39d9bb5b084d4616b53aff39aab
ayyappan1221 <plicentring9876@gmail.com>
2025-08-08 07:14:17 +0530

    Update TitleCase.java

--- String/TitleCase.java
+++ String/TitleCase.java
@@ -1,30 +1,29 @@
-
 import java.util.Scanner;
 public class TitleCase {
-   public static void main(String[]args){
-        Scanner sc=new Scanner(System.in);
-        String str=sc.nextLine();
-        int n=str.length();
-        String r="";
-        sc.close();
-        for(int i=0;i<n;i++){
-            char ch=str.charAt(i);
-            if(i==0 &&(ch>=97&&ch<=122)){
-                int t=ch-32;
-                ch=(char)t;
-                r=r+ch;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int nstr = str.length();
+        char r = ' '; 
+        for(int i = 0; i < nstr; i++) {
+            char ch = str.charAt(i);
+            if(i == 0 && (ch >= 97 && ch <= 122)) {
+                int tch = (int)ch - 32;
+                ch = (char)tch;
+                r = ch;
             }
-            else if(ch==32){r=r+ch;}
-            else if((str.charAt(i-1)==32)&&(ch>=97&&ch<=122)){
-                int t=ch-32;
-                ch=(char)t;
-                r=r+ch;
-            } 
-            else{
-                r=r+ch;
-            }   
-            
+            else if(ch == 32) {
+                r = ch;
+            }
+            else if((str.charAt(i - 1) == 32) && (ch >= 97 && ch <= 122)) {
+                int tch = (int)ch - 32; 
+                ch = (char)tch;
+                r = ch;
+            }
+            else {
+                r = ch;
+            }
         }
         System.out.print(r);
-   } 
+    }
 }
